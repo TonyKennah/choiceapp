766cb540d8001bdebbce3633ca86991f4deee1cc
TonyKennah <tonykennah@hotmail.com>
2025-07-30 15:34:35 +0100

    adding springboot

---
+++ src/main/java/uk/co/kennah/choiceapp/ChoiceappApplication.java
@@ -0,0 +1,14 @@
+package uk.co.kennah.choiceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ChoiceappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChoiceappApplication.class, args);
+    }
+
+}
